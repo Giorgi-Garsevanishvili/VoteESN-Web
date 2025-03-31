b80fb36fd792d8fcd5840c93bfb7dd30795ccf92
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-03-31 17:40:57 +0300

    loading logic added

--- auth/login.js
+++ auth/login.js
@@ -2,32 +2,46 @@ const url = "https://voteesn-api.onrender.com/api/v1/auth/login";
 
 const email = document.querySelector(".email-input");
 const password = document.querySelector(".password-input");
-const logStatus = document.querySelector(".log-status")
+const logStatus = document.querySelector(".log-status");
+const logBtn = document.querySelector(".login-btn");
 
 async function logIn(event) {
   event.preventDefault();
+
+  logBtn.disabled = true;
+  logBtn.innerHTML =
+    '<img class="password-toggle" src="../img/login/loading.gif" alt="loading..." />';
 
   const data = {
     email: email.value,
     password: password.value,
   };
-
-  await axios
-    .post(url, data)
-    .then((response) => {
-      const user = response.data
-      logStatus.innerHTML = `success: ${user.user.name}`
-    })
-    .catch((error) => {
-      logStatus.innerHTML = error.response.data.message
-    });
+  try {
+    const response = await axios.post(url, data);
+    const res = response.data;
+    const user = res.user
+    if (user.role === "admin") {
+      window.location.replace = "/dashboard";
+    } else if (user.role === "voter") {
+      window.location.replace = "/vote";
+    } else {
+      window.location.replace = "./login.html";
+    }
+  } catch (error) {
+    logStatus.innerHTML = error.response.data.message;
+  } finally {
+    logBtn.disabled = false;
+    logBtn.innerHTML = "Log In";
+  }
 }
 
-document.querySelector('.login-form').addEventListener('keydown', function(event){
-  if(event.key === "Enter"){
-    logIn(event);
-  }
-})
+document
+  .querySelector(".login-form")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      logIn(event);
+    }
+  });
 
 function togglePassword(event) {
   event.preventDefault();
