03be38089ac8a22586564e3f24d715779f2cac1f
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-04-12 18:13:39 +0300

    get results functionalyty added nececcary buttuns and chart display node package

--- src/api/accessCodes.js
+++ src/api/accessCodes.js
@@ -6,14 +6,22 @@ const toolTitle = document.querySelector(".tool-name");
 const toolContainer = document.querySelector(".tool-cont");
 const generated = document.querySelector(".generated");
 const tokCountBox = document.querySelector(".tok-count-box");
+const resBox = document.querySelector(".el-res");
 
 const genQRBtn = document.querySelector(".gen-qr-btn");
 const getQRBtn = document.querySelector(".get-qr-btn");
 
 const getResBtn = document.querySelector(".get-res-btn");
 
+let results = "";
+
 genQRBtn.addEventListener("click", async (event) => {
   event.preventDefault();
+
+  genQRBtn.classList.add("selected");
+  getQRBtn.classList.remove("selected");
+  getResBtn.classList.remove("selected");
+
   toolContainer.innerHTML = "";
   generated.innerHTML = "";
   tokCountBox.classList.remove("show");
@@ -21,8 +29,6 @@ genQRBtn.addEventListener("click", async (event) => {
 
   const elections = await getAllElection();
   const allElections = elections.data.data.allElections;
-
-  console.log(allElections);
 
   if (allElections.length > 0) {
     let optionsHTML = "";
@@ -54,6 +60,9 @@ genQRBtn.addEventListener("click", async (event) => {
 
     selector.addEventListener("change", (e) => {
       electionID.value = e.target.value;
+      clearBtn.classList.remove("show");
+      clearBtn.classList.add("hidden");
+      generated.innerHTML = "";
     });
 
     genQr.addEventListener("click", async (event) => {
@@ -100,6 +109,11 @@ genQRBtn.addEventListener("click", async (event) => {
 
 getQRBtn.addEventListener("click", async (event) => {
   event.preventDefault();
+
+  genQRBtn.classList.remove("selected");
+  getQRBtn.classList.add("selected");
+  getResBtn.classList.remove("selected");
+
   toolContainer.innerHTML = "";
   tokCountBox.classList.remove("show");
   tokCountBox.classList.add("hidden");
@@ -129,9 +143,9 @@ getQRBtn.addEventListener("click", async (event) => {
     <select class="election-selector-get">${optionsHTML}</select>
     <input disabled class="election-id-get" placeholder="Election ID:" value="${firstElection._id}">
     <button class="get-codes">Get Codes</button>
-    <button class="clear-box-get hidden">Clear</button>
     <button class="download-codes hidden">Download</button>
     <button class="delete-codes hidden">Delete</button>  
+    <button class="clear-box-get hidden">Clear</button>
   `;
 
     toolContainer.insertAdjacentHTML("afterbegin", html);
@@ -141,8 +155,8 @@ getQRBtn.addEventListener("click", async (event) => {
     const deleteBtn = document.querySelector(".delete-codes");
 
     const electionID = document.querySelector(".election-id-get");
-    const clearBtn = document.querySelector(".clear-box-get");
     const selector = document.querySelector(".election-selector-get");
+    const clearBtn = document.querySelector(".clear-box-get");
 
     selector.addEventListener("change", (e) => {
       generated.innerHTML = "";
@@ -161,6 +175,13 @@ getQRBtn.addEventListener("click", async (event) => {
       generated.innerHTML = "";
       clearBtn.classList.add("hidden");
       clearBtn.classList.remove("show");
+      generated.innerHTML = "";
+      downloadBtn.classList.remove("show");
+      downloadBtn.classList.add("hidden");
+      deleteBtn.classList.remove("show");
+      deleteBtn.classList.add("hidden");
+      tokCountBox.classList.remove("show");
+      tokCountBox.classList.add("hidden");
     });
 
     getCodesBtn.addEventListener("click", async (event) => {
@@ -173,6 +194,9 @@ getQRBtn.addEventListener("click", async (event) => {
         );
 
         const tokens = response.data.tokens;
+
+        clearBtn.classList.remove("hidden");
+        clearBtn.classList.add("show");
 
         let tokensHTML = "";
         tokens.forEach((token) => {
@@ -261,6 +285,223 @@ getQRBtn.addEventListener("click", async (event) => {
   }
 });
 
+getResBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  genQRBtn.classList.remove("selected");
+  getQRBtn.classList.remove("selected");
+  getResBtn.classList.add("selected");
+
+  toolContainer.innerHTML = "";
+  tokCountBox.classList.remove("show");
+  tokCountBox.classList.add("hidden");
+  generated.innerHTML = "";
+  toolTitle.innerHTML = "Get Results";
+
+  const electionResponse = await getAllElection();
+  const allElections = electionResponse.data.data.allElections;
+
+  const firstElection = allElections[0];
+
+  let optionsHTML = "";
+
+  allElections.forEach((election) => {
+    optionsHTML += `
+  <option value="${election._id}">${election.title}</option>
+  `;
+  });
+
+  let html = `
+  <select class="election-selector-get">${optionsHTML}</select>
+  <input disabled class="election-id-get" placeholder="Election ID:" value="${firstElection._id}">
+  <button class="get-result">Get Results</button>
+  <button class="download-result hidden">Download Full Report</button>
+  <button class="delete-result hidden">Delete</button>  
+  <button class="clear-box-result hidden">Clear</button>
+`;
+
+  toolContainer.insertAdjacentHTML("afterbegin", html);
+
+  const electionID = document.querySelector(".election-id-get");
+  const selector = document.querySelector(".election-selector-get");
+
+  const getResultBTN = document.querySelector(".get-result");
+  const downloadResultBTN = document.querySelector(".download-result");
+  const deleteResultBTN = document.querySelector(".delete-result");
+  const clearBTN = document.querySelector(".clear-box-result");
+
+  getResultBTN.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    resBox.innerHTML = "";
+    generated.innerHTML = "";
+    results = "";
+
+    await getResults(electionID.value);
+
+    if (results.length > 0) {
+      downloadResultBTN.classList.remove("hidden");
+      downloadResultBTN.classList.add("show");
+      deleteResultBTN.classList.remove("hidden");
+      deleteResultBTN.classList.add("show");
+      clearBTN.classList.remove("hidden");
+      clearBTN.classList.add("show");
+
+      const flatAnswers = results.flat();
+
+      function normalize(str) {
+        return str.trim().toLowerCase();
+      }
+
+      let stats = {};
+
+      flatAnswers.forEach(({ question, selectedOption }) => {
+        const qKey = normalize(question);
+        const aKey = normalize(selectedOption);
+
+        if (!stats[qKey]) {
+          stats[qKey] = {};
+        }
+
+        if (!stats[qKey][aKey]) {
+          stats[qKey][aKey] = 0;
+        }
+
+        stats[qKey][aKey]++;
+      });
+
+      selector.addEventListener("change", (e) => {
+        resBox.innerHTML = "";
+        results = "";
+        stats = {};
+        generated.innerHTML = "";
+        electionID.value = e.target.value;
+        downloadResultBTN.classList.remove("show");
+        downloadResultBTN.classList.add("hidden");
+        deleteResultBTN.classList.remove("show");
+        deleteResultBTN.classList.add("hidden");
+        tokCountBox.classList.remove("show");
+        tokCountBox.classList.add("hidden");
+        clearBTN.classList.remove("show");
+        clearBTN.classList.add("hidden");
+      });
+
+      let html = `<div id="charts"></div>`;
+
+      generated.insertAdjacentHTML("afterbegin", html);
+
+      const container = document.getElementById("charts");
+      let chartIndex = 0;
+
+      Object.entries(stats).forEach(([question, option]) => {
+        const chartBox = document.createElement("div");
+        chartBox.classList.add("chart-container");
+
+        const questionTitle = document.createElement("h4");
+        questionTitle.textContent = question;
+        chartBox.appendChild(questionTitle);
+
+        const canvas = document.createElement("canvas");
+        canvas.id = `chart-${chartIndex++}`;
+        chartBox.appendChild(canvas);
+
+        container.appendChild(chartBox);
+
+        const labels = Object.keys(option);
+        const values = Object.values(option);
+
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: labels,
+              data: values,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 205, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(201, 203, 207, 0.2)",
+              ],
+              borderColor: [
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+                "rgb(75, 192, 192)",
+                "rgb(54, 162, 235)",
+                "rgb(153, 102, 255)",
+                "rgb(201, 203, 207)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
+
+        const config = {
+          type: "bar",
+          data: data,
+          options: {
+            responsive: true,
+            layout: {
+              padding: {
+                top: 5,
+                bottom: 5,
+              },
+            },
+            plugins: {
+              legend: {
+                position: "bottom",
+                labels: {
+                  font: {
+                    size: 14,
+                  },
+                  generateLabels: function (chart) {
+                    const data = chart.data;
+                    return data.labels.map((label, i) => {
+                      const value = data.datasets[0].data[i];
+                      const color = data.datasets[0].backgroundColor[i];
+                      return {
+                        text: `${label}: ${value}`,
+                        fillStyle: color,
+                        strokeStyle: color,
+                        index: i,
+                      };
+                    });
+                  },
+                },
+              },
+              datalabels: {
+                color: "#000",
+                font: {
+                  weight: "bold",
+                  size: 14,
+                },
+                formatter: (value, context) => `${value}`,
+              },
+              tooltip: {
+                callbacks: {
+                  label: function (context) {
+                    const label = context.label || "";
+                    const value = context.parsed || 0;
+                    return `${label}: ${value} votes`;
+                  },
+                },
+              },
+            },
+          },
+          plugins: [ChartDataLabels],
+        };
+
+        new Chart(canvas, config);
+      });
+    } else {
+      resBox.innerHTML = "No results to display!";
+    }
+  });
+});
+
 async function generateQrCodes(data) {
   try {
     const genQr = document.querySelector(".gen-qr");
@@ -342,6 +583,20 @@ export async function deleteQrcodes(id) {
       deleteBtn.disabled = false;
       location.reload();
     }, 2000);
+  } catch (error) {
+    message(error.response.data.message);
+    console.log(error);
+  }
+}
+
+async function getResults(id) {
+  try {
+    const response = await axios.get(
+      `https://voteesn-api.onrender.com/api/v1/admin/election/${id}/results`,
+      config
+    );
+    const result = response.data.result;
+    results = result;
   } catch (error) {
     message(error.response.data.message);
     console.log(error);
